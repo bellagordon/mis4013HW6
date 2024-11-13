00041e9d89d2579173aa9003f729fb4cffd5062f
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-12 19:05:05 -0600

    Update lib2-model.php

--- lib2-model.php
+++ lib2-model.php
@@ -1,24 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Library 1</title>
-  <script>
-    function addFavoriteAnimalList() {
-      // Create the list as an HTML string
-      const items = ['lions', 'tigers', 'bears'];
-      const ul = document.createElement('ul');
-
-      // Generate <li> elements and append them to the <ul> using innerHTML
-      ul.innerHTML = items.map(item => `<li>${item}</li>`).join('');
-      
-      // Find the container and append the <ul>
-      const animalContainer = document.getElementById('animal-container');
-      animalContainer.appendChild(ul);
-    }
-  </script>
-</head>
-<body onload="addFavoriteAnimalList();">
-  <p>These are my favorite animals:</p>
-  <div id="animal-container"></div>
-</body>
+<html>
+  <head>
+    <title>Referencing HTML Elements</title>
+    <script src="dom-tools.js"></script>
+    <script>
+      function addFavoriteAnimalList() {
+        const ulElement = createUlElement(['lions', 'tigers', 'bears']);
+        const animalContainer = document.getElementById('animal-container');
+        animalContainer.appendChild(ulElement);
+      }
+    </script>
+  </head>
+  <body onload="addFavoriteAnimalList();">
+    <p>These are my favorite animals:</p>
+    <div id="animal-container"></div>
+  </body>
 </html>
