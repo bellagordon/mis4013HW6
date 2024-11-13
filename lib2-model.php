b3e31a4ac555e76a5bf5fe81f8501405dfe91c40
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-12 19:33:12 -0600

    Update lib2-model.php

--- lib2-model.php
+++ lib2-model.php
@@ -1,18 +1,33 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>Referencing HTML Elements</title>
-    <script src="dom-tools.js"></script>
-    <script>
-      function addFavoriteAnimalList() {
-        const ulElement = createUlElement(['lions', 'tigers', 'bears']);
-        const animalContainer = document.getElementById('animal-container');
-        animalContainer.appendChild(ulElement);
-      }
-    </script>
-  </head>
-  <body onload="addFavoriteAnimalList();">
-    <p>These are my favorite animals:</p>
-    <div id="animal-container"></div>
-  </body>
+<html lang="en">
+<head>
+  <title>Favorite Animals</title>
+  <script src="dom-tools.js"></script>
+  <style>
+    ul {
+      list-style-type: none;  
+      padding: 0; 
+    }
+    li {
+      font-size: 20px;  
+    }
+
+    .title-style {
+      font-size: 24px;  
+      font-weight: bold;  
+      text-align: center; 
+    }
+  </style>
+  <script>
+    function addFavoriteAnimalList() {
+      const ulElement = createUlElement(['lions', 'tigers', 'bears']);
+      const animalContainer = document.getElementById('animal-container');
+      animalContainer.appendChild(ulElement);
+    }
+  </script>
+</head>
+<body class="title-style" onload="addFavoriteAnimalList();">
+  <p>These are my favorite animals:</p>
+  <div id="animal-container"></div>
+</body>
 </html>
