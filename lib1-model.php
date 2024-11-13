836b29b02fe20f28e97dc828834e29639a949184
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-12 18:28:46 -0600

    Update lib1-model.php

--- lib1-model.php
+++ lib1-model.php
@@ -2,32 +2,36 @@
 <html lang="en">
 <head>
   <title>Library 1</title>
+  <!-- Include List.js library -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
 </head>
 <body>
+
+<div id="users">
+  <input class="search" placeholder="Search" />
+  <button class="sort" data-sort="name">Sort</button>
+  <ul class="list"></ul>
+</div>
+
 <script>
-var options = {
-  valueNames: [ 'name', 'born' ],
-  item: '<li><h3 class="name"></h3><p class="born"></p></li>'
-};
-var values = [{
-    name: 'Jonny Strömberg',
-    born: 1986
-  },
-  {
-    name: 'Jonas Arnklint',
-    born: 1985
-  },
-  {
-    name: 'Martina Elm',
-    born: 1986
-}];
+  var options = {
+    valueNames: ['name', 'born'],
+    item: '<li><h3 class="name"></h3><p class="born"></p></li>'
+  };
 
-var userList = new List('users', options, values);
+  var values = [
+    { name: 'Jonny Strömberg', born: 1986 },
+    { name: 'Jonas Arnklint', born: 1985 },
+    { name: 'Martina Elm', born: 1986 }
+  ];
 
-userList.add({
-  name: "Gustaf Lindqvist",
-  born: 1983
-});
+  var userList = new List('users', options, values);
+
+  userList.add({
+    name: "Gustaf Lindqvist",
+    born: 1983
+  });
 </script>
+
 </body>
 </html>
