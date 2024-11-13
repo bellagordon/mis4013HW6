1f5d8c6ea9768d08bf762a8a050ac8703668df6c
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-12 19:50:49 -0600

    Create lib4-model.php

---
+++ lib4-model.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Class',     4],
+          ['Eat',      2],
+          ['Study',  4],
+          ['Watch TV', 2],
+          ['Scroll on Phone', 3],
+          ['Workout', 1],
+          ['Sleep', 8]
+        ]);
+
+        var options = {
+          title: 'Daily Routine',
+          pieHole: 0.4,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="donutchart" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
