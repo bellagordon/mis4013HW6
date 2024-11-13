22ed5b3519a8717a063f84bed906fcfa8b62a85c
bellagordon <157334313+bellagordon@users.noreply.github.com>
2024-11-12 19:41:37 -0600

    Create lib3-model.php

---
+++ lib3-model.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <script src="https://cdn.jsdelivr.net/npm/p5@1.4.1/lib/p5.js"></script>
+    <script>
+      function setup() {
+        createCanvas(500, 500);
+        background(32);
+      }
+
+      function draw() {
+        fill(random(255), random(255), random(255));
+        circle(random(width), random(height), 25);
+      }
+    </script>
+  </head>
+  <body>
+    <h1>My Sketch</h1>
+  </body>
+</html>
